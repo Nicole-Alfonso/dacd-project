92c139589d4a476667c839b83525cae7b5bda4b7
Your Name <you@example.com>
2025-05-10 12:43:41 +0100

    BuilderLauncher Implementation

--- event-store-builder/src/main/java/org/example/eventstore/BuilderLauncher.java
+++ event-store-builder/src/main/java/org/example/eventstore/BuilderLauncher.java
@@ -1,4 +1,17 @@
 package org.example.eventstore;
 
 public class BuilderLauncher {
+    public static void main(String[] args) {
+        EventStoreBuilder builder = new EventStoreBuilder();
+        builder.start();
+
+        // Mantiene el proceso vivo
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
 }
